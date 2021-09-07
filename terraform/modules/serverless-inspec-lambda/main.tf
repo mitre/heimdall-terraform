83bd5061c4d47770973ce5117748ea8e2cc0fec1
Justin Kufro <jkufro@mitre.org>
2021-09-07 13:40:42 -0600

    use serverless inspec 0.15.7

--- terraform/modules/serverless-inspec-lambda/main.tf
+++ terraform/modules/serverless-inspec-lambda/main.tf
@@ -307,5 +307,5 @@ module "serverless-inspec-lambda" {
   security_groups = var.security_groups
   lambda_role_arn = aws_iam_role.InSpecRole.arn
   lambda_name     = "serverless-inspec-lambda-${var.deployment_id}"
-  image_version   = "0.15.6"
+  image_version   = "0.15.7"
 }
